018623b457b1c79df46d710c6c15e7b4b3a7f5c5
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-09-23 17:30:02 +0200

    Test heroku

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -4,4 +4,6 @@ async function loadConfig() {
     return result.json();
 }
 
-let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? "http://localhost:3000":"https://orinoco-mb.herokuapp.com";
+// let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? "http://localhost:3000":"https://orinoco-mb.herokuapp.com";
+
+let apiUrl = "https://orinoco-mb.herokuapp.com";
